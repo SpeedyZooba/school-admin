12b5e629e6ad2cdb9d68140e2abb01eb7ca9bb59
Görkem Revi <gorkemrevi@hotmail.com>
2023-11-23 19:28:58 +0300

    example of service-controller implementation

--- backend/controllers/student-c.js
+++ backend/controllers/student-c.js
@@ -2,17 +2,16 @@ const db = require("../models/student");
 const student = db.students;
 const Op = db.Sequelize.Op;
 
-// Create and Save a new Tutorial
 exports.create = (req, res) => {
-    // Validate request
+  // Request validation
   if (!req.body.title) {
     res.status(400).send({
-      message: "Content can not be empty!"
+      message: "Content not found."
     });
     return;
   }
 
-  // Create a Student
+  // Student creation
   const student = {
     StudentId: req.body.StudentId,  
     FirstName: req.body.FirstName,
@@ -25,37 +24,60 @@ exports.create = (req, res) => {
     GradDate: req.body.GradDate,
   };
 
-  // Save Tutorial in the database
+  // Saving the new entry
   student.create(student)
     .then(data => {
       res.send(data);
     })
     .catch(err => {
       res.status(500).send({
-        message:
-          err.message || "Some error occurred while creating the Student."
+        message: err.message || "Some error occurred while creating the student entry."
       });
     });
 };
 
-// Retrieve all Student with specific Fname from the database.
 exports.findAll = (req, res) => {
-        
-    student.findAll({ where: {} })
+    student.findAll()
       .then(data => {
         res.send(data);
       })
       .catch(err => {
         res.status(500).send({
-          message:
-            err.message || "Some error occurred while retrieving tutorials."
+          message: err.message || "Some error occurred while attempting to retrive the students."
         });
       });
 };
 
-// Find a single Tutorial with an id
+exports.findAllActive = (req, res) => {
+    student.findAll({ where: { IsActive: true }})
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Some error occurred while attempting to retrive active students."
+      });
+    });
+}
+
 exports.findOne = (req, res) => {
-  
+    const id = req.params.id;
+
+    student.findByPk(id)
+      .then(data => {
+        if (data) {
+          res.send(data);
+        } else {
+          res.status(404).send({
+            message: `Student with id=${id} could not be found.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while attempting to retrive the student with the id " + id
+        })
+      })
 };
 
 // Update a Tutorial by the id in the request
@@ -63,17 +85,28 @@ exports.update = (req, res) => {
   
 };
 
-// Delete a Tutorial with the specified id in the request
 exports.delete = (req, res) => {
-  
+    const id = req.params.id;
+
+    Tutorial.destroy({ where: { id: id } })
+      .then(num => {
+        if (num == 1) {
+          res.send({
+            message: "Student entry deleted successfully."
+          });
+        } else {
+          res.send({
+            message: `Failed deleting the student with id=${id}. Possibly empty entry.`
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: err.message || "Some error occurred while attempting to delete the student with the id " + id
+        });
+      });
 };
 
-// Delete all Tutorials from the database.
 exports.deleteAll = (req, res) => {
-  
-};
-
-// Find all published Tutorials
-exports.findAllPublished = (req, res) => {
   
 };
---
+++ backend/services/studentService.js
@@ -0,0 +1,98 @@
+const { Student } = require("../models/student.js");
+const { ParentOf } = require("../models/parent_of.js");
+
+async function createStudent(StudentId, FirstName, LastName, Sex, BirthDate, PhoneNo, Email, IsActive, GradDate, ParentId)
+{
+    try 
+    {
+        const student = await Student.create({ StudentId, FirstName, LastName, Sex, BirthDate, PhoneNo, Email, IsActive, GradDate, ParentId });
+        await ParentOf.create({ StudentId, ParentId });
+        return student;
+    }
+    catch (error)
+    {
+        console.error("Something went wrong with student creation.", error);
+        throw new Error("Something went wrong.");
+    }
+}
+
+async function getAllStudents()
+{
+    try 
+    {
+        const students = await Student.findAll();
+        return students;
+    }
+    catch (error)
+    {
+        console.error("Something went wrong with whole student fetching.", error);
+        throw new Error("Something went wrong.");
+    }
+}
+
+async function getActiveStudents()
+{
+    try
+    {
+        const students = await Student.findAll({ where: { IsActive: true } });
+        return students;
+    }
+    catch (error)
+    {
+        console.error("Something went wrong with active student fetching.", error);
+        throw new Error("Something went wrong.");
+    }
+}
+
+async function graduateStudent(id)
+{
+    try
+    {
+        await Student.update({ IsActive: true }, { where: { id } });
+        return "success";
+    }
+    catch (error)
+    {
+        console.error("Something went wrong with student graduation.", error);
+        throw new Error("Something went wrong.");
+    }
+}
+
+async function updateStudent(id, newData)
+{
+    try
+    {
+        await Student.update(newData, { where: { id } });
+        return "success";
+    }
+    catch (error)
+    {
+        console.error("Something went wrong with student update.", error);
+        throw new Error("Something went wrong.");
+    }
+}
+
+async function deleteStudent(id)
+{
+    try
+    {
+        await ParentOf.destroy({ where: { StudentId: id }});
+        await Student.destroy({ where: { id } });
+        return "success";
+    }
+    catch (error)
+    {
+        console.error("Something went wrong with student deletion.", error);
+        throw new Error("Something went wrong.");
+    }
+}
+
+module.exports 
+{
+    createStudent,
+    getAllStudents,
+    getActiveStudents,
+    graduateStudent,
+    updateStudent,
+    deleteStudent
+}

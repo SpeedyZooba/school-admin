7f03bef21c014f64b0361318e951e95d0b2d7303
boranozdemir <mr.boranozdemir@gmail.com>
2023-11-24 20:56:31 +0300

    Student-Parent Solved

--- backend/controllers/studentController.js
+++ backend/controllers/studentController.js
@@ -2,8 +2,8 @@ const studentService = require("../services/studentService.js");
 
 async function registerStudent(req, res) {
     try {
-        const { StudentId, FirstName, LastName, Sex, BirthDate, PhoneNo, Email, IsActive, GradDate } = req.body;
-        const student = await studentService.createStudent({ StudentId, FirstName, LastName, Sex, BirthDate, PhoneNo, Email, IsActive, GradDate });
+        const { StudentId, FirstName, LastName, Sex, BirthDate, PhoneNo, Email, IsActive, GradDate, ParentId } = req.body;
+        const student = await studentService.createStudent({ StudentId, FirstName, LastName, Sex, BirthDate, PhoneNo, Email, IsActive, GradDate }, { StudentId, ParentId });
         res.status(201).json(student);
     }
     catch (error) {
--- backend/services/studentService.js
+++ backend/services/studentService.js
@@ -3,13 +3,14 @@ const initModels = require('../models/init-models.js');
 
 const models = initModels(sequelize);
 const Student = models.student;
+const ParentOf = models.parent_of;
 
-async function createStudent(studentData)
+async function createStudent(studentData, parentOfData)
 {
     try 
     {
         const student = await Student.create(studentData);
-        await ParentOf.create({ StudentId, ParentId });
+        await ParentOf.create(parentOfData);
         return student;
     }
     catch (error)
